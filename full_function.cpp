d9760897d40af8316c7337f86017ca6382dd8c41
Hu Zhenwei <47142350+tom0727@users.noreply.github.com>
2019-04-24 21:27:27 +0800

    Update full_function.cpp

--- full_function.cpp
+++ full_function.cpp
@@ -916,6 +916,69 @@ float to_percentage (float amount){
   return (amount*100);
 }
 
+void print_graph (map <float,string> &datamap,float &income,float &expense,string x_header){
+    cout<<"Income Percentage"<<endl;
+    cout<<"A"<<endl;
+    cout<<"I"<<endl;
+    for (int j=19;j>=0;j--){
+      cout<<"I";
+      if (j%4==3){
+        cout<<setw(3)<<(j+1)*5<<"%  ";
+      }
+      else{
+        cout<<"      ";
+      }
+      for (map <float,string >::iterator itr=datamap.begin();itr!=datamap.end();itr++){
+        if (itr->first<0){continue;}
+        if ((itr->first/income)>=j*0.05){
+          cout<<"**";
+        }
+        else{cout<<"  ";};
+        cout<<"        ";
+      }
+      cout<<endl;
+    }
+   cout<<"-------------------------------------------------------------------->"<<x_header<<endl;
+   for (map <float,string >::iterator itr=datamap.begin();itr!=datamap.end();itr++){
+      if (itr->first<0){continue;}
+      cout<<setw(10)<<right<<itr->second;
+   }
+
+
+
+   cout<<endl;
+   cout<<endl;
+   cout<<"Expenditure Percentage"<<endl;
+   cout<<"A"<<endl;
+   cout<<"I"<<endl;
+   for (int j=19;j>=0;j--){
+     cout<<"I";
+     if (j%4==3){
+       cout<<setw(3)<<(j+1)*5<<"%  ";
+     }
+     else{
+       cout<<"      ";
+     }
+     for (map <float,string >::iterator itr=datamap.begin();itr!=datamap.end();itr++){
+       if (itr->first>0){continue;}
+       if ((itr->first/income)<=j*0.05){
+         cout<<"**";
+       }
+       else{cout<<"  ";};
+       cout<<"        ";
+     }
+     cout<<endl;
+   }
+  cout<<"-------------------------------------------------------------------->"<<x_header<<endl;
+  for (map <float,string >::iterator itr=datamap.begin();itr!=datamap.end();itr++){
+     if (itr->first>0){continue;}
+     cout<<setw(10)<<right<<itr->second;
+  }
+  cout<<endl;
+  cout<<endl;
+}
+
+
 void Show_Income_Expense_ByType(vector <Record> &recordList,map <string,int> &typeList,float &income,float &expense){
   map <float,string> typemap;
   for (map <string,int>::iterator itr=typeList.begin();itr!=typeList.end();itr++){
@@ -932,6 +995,7 @@ void Show_Income_Expense_ByType(vector <Record> &recordList,map <string,int> &ty
     typemap[Category_expense]=itr->first;
   }
   cout << endl;
+  print_graph(typemap,income,expense,"Type");
   for (map <float,string>::iterator itr=typemap.begin();itr!=typemap.end();itr++){
     if ((itr->first)>0){
       cout<<"The "<<left<<setw(10)<<itr->second<<"income  is +"<<fixed<<setprecision(1)<<setw(9)<<itr->first<<" occupying "<<setw(5)<<to_percentage(itr->first/income)<< "% "<<"of total income"<<endl;
@@ -962,6 +1026,7 @@ void Show_Income_Expense_ByMethod(vector <Record> &recordList,map <string,int> &
     methodmap[Category_expense]=itr->first;
   }
   cout << endl;
+  print_graph(methodmap,income,expense,"Method");
   for (map <float,string>::iterator itr=methodmap.begin();itr!=methodmap.end();itr++){
     if ((itr->first)>0){
       cout<<"The income  via "<<left<<setw(10)<<itr->second<<" is +"<<fixed<<setprecision(1)<<setw(9)<<itr->first<<" occupying "<<setw(5)<<to_percentage(itr->first/income)<<"% "<<"of total income"<<endl;
