dd8ebb6d112c71d83ca28452e5cfe15e98ddfe3a
yyuezhi <48882898+yyuezhi@users.noreply.github.com>
2019-04-27 14:25:10 +0800

    Add some comments

--- 1340Project_group32/financial_report.cpp
+++ 1340Project_group32/financial_report.cpp
@@ -11,6 +11,7 @@
 #include <map>
 #include "headerfile.h"
 using namespace std;
+//print the menu of the financial report function
 void print_menu3(){
   cout << endl;
   cout<<"1.Set budget"<<endl;
@@ -20,10 +21,14 @@ void print_menu3(){
   cout<<"5.View monthly income and expense"<<endl;
   cout<<"Please enter your choice(press Enter to Exit): ";
 }
+
+// execute the first function of financial financial_report： set the budget
+//input : the original budget user defined and the user total expense
 void set_budget(float &budget, float &expense){
   string s_budget;
   cout<<"Please enter your expected budget: ";
   getline(cin,s_budget);
+  //check whether the amount user input is in the right format
   while (!amount_check(s_budget)||s_budget[0]=='-'){
     cout<<"Invalid input, please enter again: ";
     getline(cin,s_budget);
@@ -33,12 +38,21 @@ void set_budget(float &budget, float &expense){
   if (expense>=budget) {cout<<"The current expense is: "<<expense<< " ,You have exceeded the budget of "<<budget;}
 }
 
+// helper function to format the output
+//change the decimal number to the persentage number
 float to_percentage (float amount){
   if (amount==0){return 0;}
   else{return (amount*100);}
 }
 
+// helper function the print the bar chart with
+//X-axis: the record payment method or the record payment type(category)
+//Y-axis: the persentage each category occupy the total income / Expense
+// x_header: the header of the x axis
+//datamap: A map with key: the income/expense of certain categoty or payment methodmap
+//                  value: the name of that category/ payment method
 void print_graph (map <float,string> &datamap,float &income,float &expense,string x_header){
+    // print the bar chart of different category's income
     cout<<"Income Percentage"<<endl;
     cout<<"A"<<endl;
     for (int j=21;j>=0;j--){
@@ -49,20 +63,25 @@ void print_graph (map <float,string> &datamap,float &income,float &expense,strin
       else{
         cout<<"         ";
       }
+      // print the bar
       for (map <float,string >::iterator itr=datamap.begin();itr!=datamap.end();itr++){
         if (itr->first<0){continue;}
         if ((itr->first/income)>=j*0.05){
           cout<<"**";
           cout<<"        ";
         }
+        // print number in the top of the bar to show the persentagen of that type with respect to total income
+        // and the absolute value of that type of income
         else if ((j*0.05-(itr->first/income))<0.05){cout<<fixed<<setprecision(1)<<to_percentage((itr->first)/income)<<"%     ";}
         else if (j*0.05-(itr->first/income)<0.10 && j*0.05-(itr->first/income)>0.05){cout<<fixed<<left<<setprecision(1)<<setw(10)<<itr->first;}
         else{cout<<"          ";};
       }
       cout<<endl;
     }
+    // print the X-axis and the X-axis header
    cout<<"-------------------------------------------------------------------->"<<x_header<<endl;
    cout<<"   ";
+   // print the name of each category along X-axis
    for (map <float,string >::iterator itr=datamap.begin();itr!=datamap.end();itr++){
       if (itr->first<0){continue;}
       cout<<setw(10)<<right<<itr->second;
@@ -70,8 +89,8 @@ void print_graph (map <float,string> &datamap,float &income,float &expense,strin
    cout<<endl;
    cout<<endl;
 
-
-
+   // repeat the similar process for expense
+   // print the graph of different category's expense
    cout<<"Expense Percentage"<<endl;
    cout<<"A"<<endl;
    cout<<"I"<<endl;
@@ -83,20 +102,25 @@ void print_graph (map <float,string> &datamap,float &income,float &expense,strin
      else{
        cout<<"         ";
      }
+     // print the bar
      for (map <float,string >::iterator itr=datamap.begin();itr!=datamap.end();itr++){
        if (itr->first>0){continue;}
        if ((itr->first/expense)<=(j*-0.05)){
          cout<<"**";
          cout<<"        ";
        }
+       // print number in the top of the bar to show the persentagen of that type with respect to total income
+       // and the absolute value of that type of income
        else if ((itr->first/expense)+j*0.05<0.05){cout<<fixed<<setprecision(1)<<to_percentage(-(itr->first)/expense)<<"%     ";}
        else if ((itr->first/expense)+j*0.05<0.10 && (itr->first/expense)+j*0.05>0.05){cout<<fixed<<left<<setprecision(1)<<setw(10)<<-itr->first;}
        else{cout<<"          ";};
      }
      cout<<endl;
    }
+   // print the X-axis and the X-axis header
   cout<<"-------------------------------------------------------------------->"<<x_header<<endl;
   cout<<"   ";
+  // print the name of each category along X-axis
   for (map <float,string >::iterator itr=datamap.begin();itr!=datamap.end();itr++){
      if (itr->first>0){continue;}
      cout<<setw(10)<<right<<itr->second;
@@ -107,8 +131,12 @@ void print_graph (map <float,string> &datamap,float &income,float &expense,strin
 
 
 
-
+// execute the second function of financial financial_report： show the financial report according to the usage of money
+//input: the user record, the map with key: the name of each expense/income type
+//                                with value :  the numnber of record which have that specific type
 void Show_Income_Expense_ByType(vector <Record> &recordList,map <string,int> &typeList,float &income,float &expense){
+  // generate a map with key: the total expense/income of a particular usage of income/expense
+  //                with value : the name of that type usage of income/expense
   map <float,string> typemap;
   for (map <string,int>::iterator itr=typeList.begin();itr!=typeList.end();itr++){
     float Category_income=0, Category_expense=0;
@@ -126,12 +154,15 @@ void Show_Income_Expense_ByType(vector <Record> &recordList,map <string,int> &ty
 
   cout << endl;
   string choice;
+  // print the menu to give user choice about the format to display
   cout<<"1.View by graph"<<endl;
   cout<<"2.View by Text"<<endl;
   cout<<"Please input your choice (Press Enter to exit)";
   getline(cin,choice);
   while (true){
+    // show by graph
     if (choice=="1"){print_graph(typemap,income,expense,"Type");return;}
+    // show by text
     else if (choice=="2"){
       for (map <float,string>::iterator itr=typemap.begin();itr!=typemap.end();itr++){
         if ((itr->first)>0){
@@ -150,10 +181,14 @@ void Show_Income_Expense_ByType(vector <Record> &recordList,map <string,int> &ty
 
 
 
-
+// execute the third function of financial financial_report： show the financial report according to type of method of payment
+//input: the user record, the map with key: the name of each expense/income type
+//
 void Show_Income_Expense_ByMethod(vector <Record> &recordList,map <string,int> &methodList,float &income,float &expense)
 {
   map <float,string> methodmap;
+  // generate a map with key: the total expense/income of a particular method of payment
+  //                with value : the name of that method of payment
   for (map <string,int>::iterator itr=methodList.begin();itr!=methodList.end();itr++){
     float Category_income=0, Category_expense=0;
     for (int i=0;i<recordList.size();i++){
@@ -169,12 +204,15 @@ void Show_Income_Expense_ByMethod(vector <Record> &recordList,map <string,int> &
   }
   cout << endl;
   string choice;
+  // the user would choice to view by graph and text
   cout<<"1.View by graph"<<endl;
   cout<<"2.View by Text"<<endl;
   cout<<"Please input your choice (Press Enter to exit)";
   getline(cin,choice);
   while (true){
+    // to show in graph format
     if (choice=="1"){print_graph(methodmap,income,expense,"Method");return;}
+    // to show in text format
     else if (choice=="2"){
       for (map <float,string>::iterator itr=methodmap.begin();itr!=methodmap.end();itr++){
         if ((itr->first)>0){
@@ -191,6 +229,58 @@ void Show_Income_Expense_ByMethod(vector <Record> &recordList,map <string,int> &
   }
 }
 
+// View the total income /expense in a user defined interval (specific to day)
+void Show_Income_Expense_ByDate(vector <Record> &recordList){
+  cout << endl;
+  cout << "Please enter a time interval you want to search for" << endl;
+  cout << "The format is YYYYMMDD, press Enter to exit" << endl;
+  cout << "Enter 0 to show from the earliest/latest time" << endl;
+  // prompt to input the starting date
+  cout << "Start time: ";
+  string starttime,endtime;
+  getline(cin,starttime);
+  if (starttime=="") {return;}
+  while (!time_check(starttime) && starttime!="0")  //time_check function is in another file
+  {
+    cout << "Invalid input,please enter again: ";
+    getline (cin,starttime);
+    if (starttime=="") {return;}
+  }
+  // to include the earlist possible date
+  if (starttime=="0") {starttime = "00000000";}
+  starttime=starttime.substr(0,4)+"-"+starttime.substr(4,2)+"-"+starttime.substr(6,2);
+
+  // prompt to the endding date
+  cout << "End time: ";
+  getline(cin,endtime);
+  if (endtime=="") {return;}
+  while (!time_check(endtime) && endtime!="0")  //time_check function is in another file
+  {
+    cout << "Invalid input,please enter again: " ;
+    getline (cin,endtime);
+    if (endtime=="") {return;}
+  }
+  // to include the latest possible date
+  if (endtime=="0") {endtime = "99999999";}
+  endtime=endtime.substr(0,4)+"-"+endtime.substr(4,2)+"-"+endtime.substr(6,2);
+
+  // interval_income,interval_expense: the total income /expense of the given defined interval
+  float interval_expense=0,interval_income=0;
+  for (int i=0;i<recordList.size();i++){  //assume it is a vector
+    if (recordList[i].get_time()>=starttime && recordList[i].get_time()<=endtime){
+      if (recordList[i].get_amount()>0){interval_income+=recordList[i].get_amount();}
+      if (recordList[i].get_amount()<0){interval_expense+=recordList[i].get_amount();}
+    }
+  }
+  cout << endl;
+  cout<<"The total income  in this interval is: +"<<interval_income<<endl;
+  cout<<"The total expense in this interval is: "<<interval_expense<<endl;
+}
+
+
+// helper function which help the iteration of data Format
+// to increase the date by one moneth
+// used in the fifth function: view monthly income /expenditure
 void date_increment(string &date){
   if (date.substr(5,2)=="12"){
     date[5]='0';
@@ -208,7 +298,11 @@ void date_increment(string &date){
   else{date[6]+=1;}
 }
 
+// helper function for the fifth function which would help to print the bar chart
+// the X-axis: the date_increment
+// the Y-axis: the income/ expenditure of that month(seperately)
 void print_graph2 (map <string,float> &timemap_income,map <string,float> &timemap_expense,float &income,float &expense,string x_header){
+    // print the bar chart of the monthly income first
     cout<<"Income Percentage"<<endl;
     cout<<"A"<<endl;
     for (int j=21;j>=0;j--){
@@ -217,26 +311,31 @@ void print_graph2 (map <string,float> &timemap_income,map <string,float> &timema
         cout<<setw(3)<<(j+1)*5<<"%     ";
       }
       else{cout<<"         ";}
+      // print the bar
       for (map <string,float>::iterator itr=timemap_income.begin();itr!=timemap_income.end();itr++){
         if ((itr->second/income)>j*0.05){
           cout<<"**";
           cout<<"        ";
         }
+        // print the absolute value of income of each month above each month's bar
+        // print the persentage of that month income with respective to total income of the period above each month of the bar
         else if ((j*0.05-(itr->second/income))<0.05){cout<<fixed<<setprecision(1)<<left<<setw(5)<<to_percentage((itr->second)/income)<<"%    ";}
         else if (j*0.05-(itr->second/income)<0.10 && j*0.05-(itr->second/income)>=0.05){cout<<fixed<<left<<setprecision(1)<<setw(10)<<itr->second;}
         else{cout<<"          ";};
       }
       cout<<endl;
     }
+    //print the x-axis
    cout<<"-------------------------------------------------------------------->"<<x_header<<endl;
    cout<<"    ";
+   // print the month along the x-axis
    for (map <string,float >::iterator itr=timemap_income.begin();itr!=timemap_income.end();itr++){
       cout<<setw(10)<<right<<itr->first;
    }
    cout<<endl;
    cout<<endl;
 
-
+  // print the bar chart of the monthly expense
    cout<<"Expense Percentage"<<endl;
    cout<<"A"<<endl;
    cout<<"I"<<endl;
@@ -248,19 +347,24 @@ void print_graph2 (map <string,float> &timemap_income,map <string,float> &timema
      else{
        cout<<"         ";
      }
+     // print the bar
      for (map <string,float >::iterator itr=timemap_expense.begin();itr!=timemap_expense.end();itr++){
        if ((itr->second/expense)<(j*-0.05)){
          cout<<"**";
          cout<<"        ";
        }
+       // print the absolute value of expense of each month above each month's bar
+       // print the persentage of that month expense with respective to total expense of the period above each month of the bar
        else if ((itr->second/expense)+j*0.05<0.05){cout<<fixed<<setprecision(1)<<left<<setw(5)<<to_percentage(-(itr->second/expense))<<"%    ";}
        else if ((itr->second/expense)+j*0.05<0.10 && (itr->second/expense)+j*0.05>=0.05){cout<<fixed<<left<<setprecision(1)<<setw(10)<<((itr->second==0)? (itr->second):(-itr->second));}
        else{cout<<"          ";};
      }
      cout<<endl;
    }
+  // print the X-axis
   cout<<"-------------------------------------------------------------------->"<<x_header<<endl;
   cout<<"    ";
+  // print the month along x-axis
   for (map <string,float >::iterator itr=timemap_expense.begin();itr!=timemap_expense.end();itr++){
      cout<<setw(10)<<right<<itr->first;
   }
@@ -268,12 +372,14 @@ void print_graph2 (map <string,float> &timemap_income,map <string,float> &timema
   cout<<endl;
 }
 
+// execute the fifth function of financial financial_report： show the financial report of monthly income/expense
+//input: the user record
 void Show_Income_Expense_ByMonth(vector <Record> &recordList){
   cout << endl;
   cout << "Please enter a monthly time interval you want to search for" << endl;
   cout << "The format is YYYYMM, press Enter to exit" << endl;
 
-
+  // prompt user to input the starting month
   cout << "Start time: ";
   string starttime,endtime;
   getline(cin,starttime);
@@ -286,6 +392,7 @@ void Show_Income_Expense_ByMonth(vector <Record> &recordList){
   }
   starttime=starttime.substr(0,4)+"-"+starttime.substr(4,2);
 
+  // prompt user to input the ending month
   cout << "End time: ";
   getline(cin,endtime);
   if (endtime=="") {return;}
@@ -297,10 +404,13 @@ void Show_Income_Expense_ByMonth(vector <Record> &recordList){
   }
   endtime=endtime.substr(0,4)+"-"+endtime.substr(4,2);
 
-
+  // total_income/total_expense: the total income /expense in the interval
   float total_income=0,total_expense=0;
+  //gernerate two map with key:each month
+  //                     value:the income/ expense of each month
   map <string,float> timemap_income,timemap_expense;
   for (string  i=starttime;i<=endtime;date_increment(i)){  //assume it is a vector
+    // interval_income,interval_expense: the monthly income /expense
     float interval_expense=0,interval_income=0;
     for (int j=0;j<recordList.size();j++){
       if (recordList[j].get_time().substr(0,7)==i){
@@ -314,13 +424,16 @@ void Show_Income_Expense_ByMonth(vector <Record> &recordList){
     total_expense-=interval_expense;
   }
   cout << endl;
+  // give user choice to view by graph or text
   string choice;
   cout<<"1.View by Graph"<<endl;
   cout<<"2.View by Text"<<endl;
   cout<<"Please input your choice (Press Enter to exit)";
   getline(cin,choice);
   while (true){
+    //view by graph
     if (choice=="1"){print_graph2(timemap_income,timemap_expense,total_income,total_expense,"Time");return;}
+    // view by text
     else if (choice=="2"){
       for (map <string,float>::iterator itr=timemap_income.begin();itr!=timemap_income.end();itr++){
         cout<<"The montly income in "<<itr->first<<" is "<<fixed<<setprecision(1)<<itr->second<<endl;
@@ -336,59 +449,17 @@ void Show_Income_Expense_ByMonth(vector <Record> &recordList){
   }
 }
 
-void Show_Income_Expense_ByDate(vector <Record> &recordList){
-  cout << endl;
-  cout << "Please enter a time interval you want to search for" << endl;
-  cout << "The format is YYYYMMDD, press Enter to exit" << endl;
-  cout << "Enter 0 to show from the earliest/latest time" << endl;
-  cout << "Start time: ";
-  string starttime,endtime;
-  getline(cin,starttime);
-
-  if (starttime=="") {return;}
-  while (!time_check(starttime) && starttime!="0")  //time_check function is in another file
-  {
-    cout << "Invalid input,please enter again: ";
-    getline (cin,starttime);
-    if (starttime=="") {return;}
-  }
-  if (starttime=="0") {starttime = "00000000";}
-  starttime=starttime.substr(0,4)+"-"+starttime.substr(4,2)+"-"+starttime.substr(6,2);
-
-  cout << "End time: ";
-  getline(cin,endtime);
-  if (endtime=="") {return;}
-  while (!time_check(endtime) && endtime!="0")  //time_check function is in another file
-  {
-    cout << "Invalid input,please enter again: " ;
-    getline (cin,endtime);
-    if (endtime=="") {return;}
-  }
-  if (endtime=="0") {endtime = "99999999";}
-  endtime=endtime.substr(0,4)+"-"+endtime.substr(4,2)+"-"+endtime.substr(6,2);
-
-  float interval_expense=0,interval_income=0;
-  for (int i=0;i<recordList.size();i++){  //assume it is a vector
-    if (recordList[i].get_time()>=starttime && recordList[i].get_time()<=endtime){
-      if (recordList[i].get_amount()>0){interval_income+=recordList[i].get_amount();}
-      if (recordList[i].get_amount()<0){interval_expense+=recordList[i].get_amount();}
-    }
-  }
-  cout << endl;
-  cout<<"The total income  in this interval is: +"<<interval_income<<endl;
-  cout<<"The total expense in this interval is: "<<interval_expense<<endl;
-}
-
-
-
+// the main funciton for financial report, which links to individual sub-function
 void financial_report(vector <Record> &recordList,map <string,int> &typeList, map <string,int> &methodList,float &income,float &expense,float &budget){
   cout << endl;
   cout<<"You have entered the financial report mode"<<endl;
   while (true)
   {
     string choice;
+    // print the menu for financial report
     print_menu3();
     getline(cin,choice);
+    // based on user's choice to enter different subfucntion
     if (choice=="1"){set_budget(budget,expense);}
     else if (choice=="2"){Show_Income_Expense_ByType(recordList,typeList,income,expense);}
     else if (choice=="3"){Show_Income_Expense_ByMethod(recordList,methodList,income,expense);}
