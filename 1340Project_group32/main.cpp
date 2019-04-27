38439b672993b17f5d472466938bfb0d4325d718
yyuezhi <48882898+yyuezhi@users.noreply.github.com>
2019-04-27 14:32:31 +0800

    add more comment

--- 1340Project_group32/main.cpp
+++ 1340Project_group32/main.cpp
@@ -14,14 +14,23 @@ using namespace std;
 
 int main()
 { string login_user="",user_password="";
-  float budget=9999999999,income,expense;
+ // income , expense,budget: the total income /expense/budget regardless of date
+  float budget,income,expense;
+ // RecordList: a list which stores record loaded form file
   vector<Record> recordList;
+ // typeList: Key: the name of differnet usage of money
+ //         value: the number of record of that type of usage of money
+ //methodList: Key: the name of differnet payment method
+ //         value: the number of record of that category of payment of method
   map <string,int> typeList;
   map <string,int> methodList;
+ // Login to the system
   login(login_user,user_password,budget,income,expense);
+ // load the data stores in username_record.txt file to different list and variable
   load_userdata(login_user,recordList,typeList,methodList);
   // The overall menu of function avaliable for execution after login
   string choice;
+ // the main menu avaliable to perform different function
   while (true)
   {
     print_menu2();
